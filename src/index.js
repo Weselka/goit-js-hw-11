02dcf74b8ef5d6285232f1c95e14f13171f82916
Weselka <weselyna.shvets@gmail.com>
2022-10-29 00:25:03 +0300

    changes functions

--- src/index.js
+++ src/index.js
@@ -18,105 +18,33 @@ refs.searchBox.addEventListener(
   debounce(onInputSearch, DEBOUNCE_DELAY)
 );
 
-// apiRings().then(data => {
-
-// } console.log(markup(data.hits)));
-
 function onInputSearch(event) {
   event.preventDefault();
 
   const inputSearch = event.target.value.trim().toLowerCase();
   console.log(inputSearch);
 
-  // if (inputSearch.length === 0) {
-  //   return Notify.failure(
-  //     'Sorry, there are no images matching your search query. Please try again.'
-  //   );
-  // }
+  if (inputSearch.length === 0) {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
 
-//   fetchCountries(inputSearch)
-//     .then(hits => {
-//       // clearMarkup();
-//       if (hits) {
-//         renderGaleryItem(hits);
-//       }
-//       // else {
-//       //   Notify.info(
-//       //     'Sorry, there are no images matching your search query. Please try again.'
-//       //   );
-//       // }
-//     })
-//     .catch(onFetchError);
-// }
-
-apiRings(inputSearch).then(data => {
-  refs.container.insertAdjacentHTML('beforeend', (markup(data.hits)))
-});
+  apiRings(inputSearch)
+    .then(data => {
+      refs.container.insertAdjacentHTML('beforeend', markup(data.hits));
+    })
+    .catch(onFetchError);
+}
 
 function clearMarkup() {
   refs.container.innerHTML = '';
 }
-
-// function renderGaleryItem([
-//   { webformatURL, tags, likes, views, comments, downloads },
-// ]) {
-//   const markupCard = `<div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes ${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views ${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments ${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads ${downloads}</b>
-//     </p>
-//   </div>
-// </div>`;
-//   console.log(markupCard);
-//   refs.container.innerHTML = markupCard;
-// }
-
-// function renderGaleryItem(data) {
-//   console.log(data);
-//   const markupCard = data.hits
-//     .map(
-//       ({
-//         webformatURL,
-//         userImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `<a href="${userImageURL}"><div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes ${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views ${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments ${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads ${downloads}</b>
-//     </p>
-//   </div>
-// </div></a>`
-//     )
-//     .joiun('');
-//   refs.container.innerHTML = markupCard;
-// }
 
 function onFetchError(error) {
   Notify.failure(
     'Sorry, there are no images matching your search query. Please try again.'
   );
 }
+
+//Notify.info('Sorry, there are no images matching your search query. Please try again.')
