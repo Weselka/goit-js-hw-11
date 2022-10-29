abb1253bf48bc288cb663f407d55b4d0d4e81630
Weselka <weselyna.shvets@gmail.com>
2022-10-29 11:36:03 +0300

    add page in api

--- src/js/apiRings.js
+++ src/js/apiRings.js
@@ -6,8 +6,8 @@ const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
 //   },
 // };
 
-export function apiRings(name) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&lim&page=1&per_page=40`;
+export function apiRings(name, page = 1) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${name}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&lim&page=1&per_page=40`;
   return fetch(url).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
