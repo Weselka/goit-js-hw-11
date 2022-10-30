9c7070ea185b7ec96d5d2fe8d73e0cbf8f57b911
Weselka <weselyna.shvets@gmail.com>
2022-10-30 03:59:37 +0300

    add listner on searchBtn

--- src/index.js
+++ src/index.js
@@ -8,44 +8,79 @@ import markup from './js/templates/markup.hbs';
 // const DEBOUNCE_DELAY = 3000;
 
 const refs = {
-  body: document.querySelector('body'),
   searchBox: document.querySelector('#search-form'),
+  searchBtn: document.querySelector('.search-btn'),
   container: document.querySelector('.gallery'),
-  loadMore: document.querySelector('.load-more'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
+console.log(refs)
+
 let page = 1;
+let inputSearch = '';
 
 refs.searchBox.addEventListener('input', onInputSearch);
-refs.loadMore.addEventListener('click', onLoad);
+refs.searchBox.addEventListener('click', onClickSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.searchBox.addEventListener('submit', onSubmitSearch);
+refs.searchBtn.addEventListener('click', onClickSearch);
+
+refs.searchBtn.disabled = true;
 
 function onInputSearch(event) {
   event.preventDefault();
-
-  const inputSearch = event.target.value.trim().toLowerCase();
+  clearMarkup();
+  
+  inputSearch = event.target.value;
   console.log(inputSearch);
 
   if (inputSearch.length === 0) {
+    refs.loadMoreBtn.classList.add('is-hidden');
     return Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
+  refs.searchBtn.disabled = false;
+  // apiRings(inputSearch)
+  //   .then(data => {
+  //     refs.container.insertAdjacentHTML('beforeend', markup(data.hits));
+  //   })
+  //   .catch(onFetchError);
+  // refs.loadMoreBtn.classList.remove('is-hidden');
+}
 
+function onClickSearch() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
+
+function onSubmitSearch(event) {
+  event.preventDefault();
   apiRings(inputSearch)
-    .then(data => {
-      refs.container.insertAdjacentHTML('beforeend', markup(data.hits));
+    .then(response => {
+      refs.container.insertAdjacentHTML('beforeend', markup(response.hits));
+      Notify.success(`Hooray! We found ${response.total}`);
+    })
+    .catch(onFetchError);
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
+
+function onLoadMore() {
+  page += 1;
+  paramSearch = inputSearch;
+  
+  apiRings(paramSearch, page)
+    .then(response => {
+      if (response.total === response.totalHits) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        return Notify.success(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      refs.container.insertAdjacentHTML('beforeend', markup(response.hits));
+      Notify.success(`Hooray! We found ${response.total}`);
     })
     .catch(onFetchError);
 }
-
-// function onLoad() {
-//   page += 1;
-//   apiRings(page, inputSearch)
-//     .then(data => {
-//       refs.container.insertAdjacentHTML('beforeend', markup(data.hits));
-//     })
-//     .catch(onFetchError);
-// }
 
 function clearMarkup() {
   refs.container.innerHTML = '';
@@ -56,5 +91,3 @@ function onFetchError(error) {
     'Sorry, there are no images matching your search query. Please try again.'
   );
 }
-
-//Notify.info('Sorry, there are no images matching your search query. Please try again.')
