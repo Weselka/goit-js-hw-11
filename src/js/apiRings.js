c5c9c339e32cacfc22330d9543e4913af6654c60
Weselka <weselyna.shvets@gmail.com>
2022-10-30 06:23:26 +0200

    clear fn

--- src/js/apiRings.js
+++ src/js/apiRings.js
@@ -1,27 +1,11 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
-// const options = {
-//   headers: {
-//     Authorization: API_KEY,
-//   },
-// };
 
-export function apiRings(inputSearch, page) {
+export async function apiRings(inputSearch, page = 1) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    console.log(response);
-    return response.json();
-  });
+  const response = await axios.get(url);
+  console.log(response);
+  return response.data;
 }
-
-// export async function apiRings(inputSearch, page = 1) {
-//   const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`;
-//   const responce = await axios.get(
-//     `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-//   );
-//   console.log(responce.hits);
-//   return responce.hits;
-//   }
