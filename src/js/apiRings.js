dfaa5ae3e95737d60ce9d24f1785659bc2648fb2
Weselka <weselyna.shvets@gmail.com>
2022-10-31 12:58:47 +0200

    changes place const default

--- src/js/apiRings.js
+++ src/js/apiRings.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 // const axios = require('axios').default;
-const axios = require('axios');
+
 
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
 
 export async function apiRings(inputSearch, page = 1) {
+  const axios = require('axios').default;
   const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
   const response = await axios.get(url);
   console.log(response);
