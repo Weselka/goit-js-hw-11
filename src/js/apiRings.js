42639d3afbbf19f22de34ece64f474cc5e765a10
Weselka <weselyna.shvets@gmail.com>
2022-10-31 20:54:31 +0200

    search bag v-2

--- src/js/apiRings.js
+++ src/js/apiRings.js
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
 
 export async function apiRings(inputSearch, page = 1) {
