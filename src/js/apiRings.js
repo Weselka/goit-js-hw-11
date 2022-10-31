1e52a8b6e1829604380537bdc2750d1b51ae08bb
Weselka <weselyna.shvets@gmail.com>
2022-10-31 14:38:49 +0200

    update URL for function

--- src/js/apiRings.js
+++ src/js/apiRings.js
@@ -5,8 +5,28 @@ const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
 
 export async function apiRings(inputSearch, page = 1) {
   const axios = require('axios').default;
-  const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-  const response = await axios.get(url);
+  const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}`;
+  const response = await axios.get(url, {
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   Authorization: '${API_KEY}',
+    // },
+    params: {
+      // q: 'inputSearch',
+      // page: 'page',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: '40',
+    },
+  });
   console.log(response);
   return response.data;
 }
+
+// const axios = require('axios').default;
+//   const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
+//   const response = await axios.get(url);
+//   console.log(response);
+//   return response.data;
+// }
