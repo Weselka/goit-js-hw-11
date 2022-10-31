8f730f289fa0d2c69932f758c68aa5317ffd8f80
Weselka <weselyna.shvets@gmail.com>
2022-10-31 21:04:05 +0200

    return axios import

--- src/js/apiRings.js
+++ src/js/apiRings.js
@@ -1,10 +1,10 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30883328-4550d73a5a5d91ad50d778095';
 
 export async function apiRings(inputSearch, page = 1) {
-  const axios = require('axios').default;
+  // const axios = require('axios').default;
   const url = `${BASE_URL}?key=${API_KEY}&q=${inputSearch}&page=${page}`;
   const response = await axios.get(url, {
     // headers: {
